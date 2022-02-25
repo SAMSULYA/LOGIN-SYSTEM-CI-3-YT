b198d927c0cdc4e7db4b615186bb76c8dc13e859
SAMSULYA <sulsam2000@gmail.com>
2022-02-25 15:00:15 +0700

    view validation register features

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,18 +3,44 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
     public function index()
     {
         $this->load->view('templates/auth_header');
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
-    
+
     public function registration()
     {
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
-        
+        $this->validation();
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            echo "register berhasil";
+        }
+    }
+
+    private function validation()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->validation_password();
+    }
+
+    private function validation_password()
+    {
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[5]|matches[password2]', [
+            'matches' => 'password dont match!',
+            'min_length' => 'password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|min_length[5]|matches[password1]');
     }
 }
