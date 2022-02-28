f225b5ff050382da4b31d42d06f473d9ab71e273
SAMSULYA <sulsam2000@gmail.com>
2022-02-28 19:57:45 +0700

    login features

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,10 +10,16 @@ class Auth extends CI_Controller
     }
     public function index()
     {
-        $data['title'] = 'Login Page';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+        $this->login_validation();
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Login Page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+            // success validation
+            $this->success_login_validation();
+        }
     }
 
     public function registration()
@@ -42,6 +48,12 @@ class Auth extends CI_Controller
         }
     }
 
+    private function login_validation()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+    }
+
     private function validation()
     {
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
@@ -58,5 +70,52 @@ class Auth extends CI_Controller
             'min_length' => 'password too short!'
         ]);
         $this->form_validation->set_rules('password2', 'Password', 'required|trim|min_length[5]|matches[password1]');
+    }
+
+    private function success_login_validation()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email,])->row_array();
+
+        //usernya ada
+        if ($user) {
+            //jika usernya aktif
+            if ($user['is_active'] == 1) {
+                //cek password
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id'],
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('user');
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                    Wrong Password !!</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                This Email has not been activated!!</div>');
+                redirect('auth');
+            }
+        } else {
+            # usernya gak ada
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Email is not Registered!!</div>');
+            redirect('auth');
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            You Have been Logout</div>');
+        redirect('auth');
     }
 }
